1473ae3641d99356e48160e777b6dc43701cac64
uslessFl <denisbar332@gmail.com>
2024-04-11 13:18:35 +0300

    asd

---
+++ 2.cs
@@ -0,0 +1,30 @@
+namespace calculator{
+    internal class Programm {
+        static void Main(string[] args){
+            Console.Writeline(a);
+            double a = double.Parse(Console.Readline());
+            Console.Writeline(b);
+            double b = double.Parse(Console.Readline());
+            Console.Writeline(znak?);
+            string c = Console.Readline();
+            c = Char.Parse(c);
+            if ( c== '+'){
+                Console.Writeline(a+b);
+            }
+            else if(c == '-'){
+                Console.Writeline(a-b);
+            }
+           else if(c == '*'){
+                Console.Writeline(a*b);
+            }
+            else if(c == '/'){
+                Console.Writeline(a/b);
+            }
+            else{
+                Console.Writeline('ERROR');
+            }
+        }
+    }
+        
+}
+    
